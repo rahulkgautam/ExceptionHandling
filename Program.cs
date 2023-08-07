88effe1538b02c59ebb5e651f99ad98a8ec8f2bd
rahulkgautam <rahulkgautam125@gmail.com>
2023-08-07 23:19:54 +0530

    Welcome to exception handling

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Exception handling Problem");
+        }
+    }
+}
